c432a1dbe5586b63bbb389cd3e83c2faf72fdf8a
TristanTrvl <thoravaltristan@gmail.com>
2020-09-29 12:08:50 +0200

    Edit: House now dependent of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_one :house
+  has_one :house, :dependent => :destroy
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
