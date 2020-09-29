be425018a8e13abf192e9ec4a77124fda7a975c0
TristanTrvl <thoravaltristan@gmail.com>
2020-09-29 12:07:18 +0200

    Edit: ressources :house -> ressource :house, more appropriate for 1 to 1 relationship

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, param: :_id do
-    resources :houses
+    resource :house
   end
   post '/auth/login', to: 'authentication#login'
   get '/*a', to: 'application#not_found'
