a4b82846cd5332eb8f55423729e8031c71ab28e0
TristanTrvl <thoravaltristan@gmail.com>
2020-09-29 12:09:29 +0200

    Add: CRUD finalized

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,11 +1,44 @@
 class HousesController < ApplicationController
+  before_action :authorize_request
+  before_action :find_house, except: :create
+ 
+  # GET /users/{id}/house
+  def show
+    render json: @house, status: :ok
+  end
+
+  # POST /users/{id}/house
   def create
     @user = User.find(params[:user_id])
     @house = @user.create_house(house_params)
-    render json: @house, status: :ok
+    if @house.save
+      render json: @house, status: :created
+    else
+      render json: { errors: @house.errors.full_messages },
+            status: :unprocessable_entity
+    end
   end
- 
+
+  # PUT /users/{id}/house
+  def update
+    unless @house.update(house_params)
+      render json: { errors: @house.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /users/{id}/house
+  def destroy
+    @house.destroy
+  end
+
   private
+    def find_house
+      @house = User.find_by_id!(params[:user__id]).house
+      rescue ActiveRecord::RecordNotFound
+        render json: { errors: 'House not found' }, status: :not_found
+    end
+
     def house_params
       params.permit(:address, :nb_slots, :user_id)
     end
