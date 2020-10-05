b93a21ca056e033f7811c0598c80aaadc90d0039
TristanTrvl <thoravaltristan@gmail.com>
2020-10-05 09:27:21 +0200

    Edit: Now delete associations on item destruction

--- app/models/convenience.rb
+++ app/models/convenience.rb
@@ -1,4 +1,5 @@
 class Convenience < ApplicationRecord
-  has_many :houses_has_convenience
+  has_many :houses_has_convenience, :dependent => :destroy
   has_many :houses, :through => :houses_has_convenience
+  validates :name, presence:true, uniqueness: true
 end
