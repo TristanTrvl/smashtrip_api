96f71ca9208315ad1e77bc654510e04d54e88b43
TristanTrvl <thoravaltristan@gmail.com>
2020-10-06 09:28:01 +0200

    Edit: Clean models of validations for future validation centric sprint

--- app/models/convenience.rb
+++ app/models/convenience.rb
@@ -1,5 +1,4 @@
 class Convenience < ApplicationRecord
   has_many :houses_has_convenience, :dependent => :destroy
   has_many :houses, :through => :houses_has_convenience
-  validates :name, presence:true, uniqueness: true
 end
--- app/models/house.rb
+++ app/models/house.rb
@@ -2,6 +2,4 @@ class House < ApplicationRecord
   belongs_to :user
   has_many :houses_has_convenience, :dependent => :destroy
   has_many :conveniences, :through => :houses_has_convenience
-  validates :address, presence: true
-  validates :nb_slots, format: { with: /[0-9]+/, message: "only allows numbers" }
 end
